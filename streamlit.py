ef42445d583eb4218b2676585f14bd41c70922fe
Eliseo <73314455+Eli-2020@users.noreply.github.com>
2022-07-28 15:53:30 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -19,11 +19,11 @@ with dataset:
     
     st.subheader('Total Valuation of Unicorns by Country')
     countries = pd.DataFrame(data_unicorn.groupby(["country"])["value"].sum()).head(50)
-    st.barh_chart(countries)
+    st.bar_chart(countries)
     
     st.subheader('Total Valuation by Industry')
     industry_value = pd.DataFrame(data_unicorn.groupby(["industry"])["value"].sum()).head(50)
-    st.barh_chart(data=industry_value)
+    st.bar_chart(data=industry_value)
         
     st.subheader('Choose the Best combination between date and age range for you')
     
